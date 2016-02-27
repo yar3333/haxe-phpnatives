d78cbb0396180310f0818d5860f69c35ef49d6b6
Yaroslav Sivakov <yar3333@gmail.com>
2016-02-28 01:37:34 +0300

    * outcontrol: fix rest arguments

--- library/php/OutcontrolNatives.hx
+++ library/php/OutcontrolNatives.hx
@@ -4,9 +4,17 @@ class OutcontrolNatives
 {
 	public static inline function flush() : Void return untyped __call__('flush');
 	
-	public static inline function header(str:String, replace=true) : Void return untyped __call__('header', str, replace);
-	
-	public static inline function header_ex(str:String, replace=true, http_response_code:Int) : Void return untyped __call__('header', str, replace, http_response_code);
+	public static function header(str:String, replace=true, ?http_response_code:Int) : Void
+	{
+		if (untyped __physeq__(http_response_code, null))
+		{
+			untyped __call__('header', str, replace);
+		}
+		else
+		{
+			untyped __call__('header', str, replace, http_response_code);
+		}
+	}
 	
 	public static inline function setcookie(name:String, value="", expire=0, path="", domain="", secure=false, httponly=false) : Bool return untyped __call__('setcookie', name, value, expire, path, domain, secure, httponly);
 	
@@ -36,9 +44,24 @@ class OutcontrolNatives
 	
 	public static inline function ob_list_handlers() : NativeArray return untyped __call__('ob_list_handlers');
 	
-	public static inline function ob_start(?output_callback:String->Int->String, chunk_size=0) : Bool return untyped __call__('ob_start', output_callback, chunk_size);
-	
-	public static inline function ob_start_ex(?output_callback:String->Int->String, chunk_size=0, flags:Int) : Bool return untyped __call__('ob_start', output_callback, chunk_size, flags);
+	public static function ob_start(?output_callback:String->Int->String, chunk_size=0, ?flags:Int) : Bool
+	{
+		if (untyped __physeq__(flags, null))
+		{
+			if (untyped __physeq__(output_callback, null))
+			{
+				return untyped __call__('ob_start');
+			}
+			else
+			{
+				return untyped __call__('ob_start', output_callback, chunk_size);
+			}
+		}
+		else
+		{
+			return untyped __call__('ob_start', output_callback, chunk_size, flags);
+		}
+	}
 	
 	public static inline function output_add_rewrite_var(name:String, value:String) : Bool return untyped __call__('output_add_rewrite_var', name, value);
 	
