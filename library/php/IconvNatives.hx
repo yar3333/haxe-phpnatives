89558805ec7682e8988718ccf0bb8e289f1687e1
Yaroslav Sivakov <yar3333@gmail.com>
2016-02-28 01:14:27 +0300

    * iconv: fix rest arguments+

--- library/php/IconvNatives.hx
+++ library/php/IconvNatives.hx
@@ -6,21 +6,98 @@ class IconvNatives
 	
 	public static inline function iconv_get_encoding(type="all") : Dynamic return untyped __call__('iconv_get_encoding', type);
 	
-	public static inline function iconv_mime_decode_headers(encoded_headers:String, mode=0, ?charset:String) : NativeArray return untyped __call__('iconv_mime_decode_headers', encoded_headers, mode, charset);
+	public static function iconv_mime_decode_headers(encoded_headers:String, mode=0, ?charset:String) : NativeArray
+	{
+		if (untyped __physeq__(charset, null))
+		{
+			return untyped __call__('iconv_mime_decode_headers', encoded_headers, mode);
+		}
+		else
+		{
+			return untyped __call__('iconv_mime_decode_headers', encoded_headers, mode, charset);
+		}
+	}
 	
-	public static inline function iconv_mime_decode(encoded_header:String, mode=0, ?charset:String) : String return untyped __call__('iconv_mime_decode', encoded_header, mode, charset);
+	public static function iconv_mime_decode(encoded_header:String, mode=0, ?charset:String) : String
+	{
+		if (untyped __physeq__(charset, null))
+		{
+			return untyped __call__('iconv_mime_decode', encoded_header, mode);
+		}
+		else
+		{
+			return untyped __call__('iconv_mime_decode', encoded_header, mode, charset);
+		}
+	}
 	
-	public static inline function iconv_mime_encode(field_name:String, field_value:String, ?preferences:NativeArray) : String return untyped __call__('iconv_mime_encode', field_name, field_value, preferences);
+	public static function iconv_mime_encode(field_name:String, field_value:String, ?preferences:NativeArray) : String
+	{
+		if (untyped __physeq__(preferences, null))
+		{
+			return untyped __call__('iconv_mime_encode', field_name, field_value);
+		}
+		else
+		{
+			return untyped __call__('iconv_mime_encode', field_name, field_value, preferences);
+		}
+	}
 	
 	public static inline function iconv_set_encoding(type:String, charset:String) : Bool return untyped __call__('iconv_set_encoding', type, charset);
 	
-	public static inline function iconv_strlen(str:String, ?charset:String) : Int return untyped __call__('iconv_strlen', str, charset);
+	public static function iconv_strlen(str:String, ?charset:String) : Int
+	{
+		if (untyped __physeq__(charset, null))
+		{
+			return untyped __call__('iconv_strlen', str);
+		}
+		else
+		{
+			return untyped __call__('iconv_strlen', str, charset);
+		}
+	}
 	
-	public static inline function iconv_strpos(haystack:String, needle:String, offset=0, ?charset:String) : Int return untyped __call__('iconv_strpos', haystack, needle, offset, charset);
+	public static function iconv_strpos(haystack:String, needle:String, offset=0, ?charset:String) : Int
+	{
+		if (untyped __physeq__(charset, null))
+		{
+			return untyped __call__('iconv_strpos', haystack, needle, offset);
+		}
+		else
+		{
+			return untyped __call__('iconv_strpos', haystack, needle, offset, charset);
+		}
+	}
 	
-	public static inline function iconv_strrpos(haystack:String, needle:String, ?charset:String) : Int return untyped __call__('iconv_strrpos', haystack, needle, charset);
+	public static function iconv_strrpos(haystack:String, needle:String, ?charset:String) : Int
+	{
+		if (untyped __physeq__(charset, null))
+		{
+			return untyped __call__('iconv_strrpos', haystack, needle);
+		}
+		else
+		{
+			return untyped __call__('iconv_strrpos', haystack, needle, charset);
+		}
+	}
 	
-	public static inline function iconv_substr(str:String, offset:Int, ?length:Int, ?charset:String) : String return untyped __call__('iconv_substr', str, offset, length, charset);
+	public static function iconv_substr(str:String, offset:Int, ?length:Int, ?charset:String) : String
+	{
+		if (untyped __physeq__(charset, null))
+		{
+			if (untyped __physeq__(length, null))
+			{
+				return untyped __call__('iconv_substr', str, offset);
+			}
+			else
+			{
+				return untyped __call__('iconv_substr', str, offset, length);
+			}
+		}
+		else
+		{
+			return untyped __call__('iconv_substr', str, offset, length, charset);
+		}
+	}
 	
 	public static inline function iconv(in_charset:String, out_charset:String, str:String) : String return untyped __call__('@iconv', in_charset, out_charset, str);
 	
