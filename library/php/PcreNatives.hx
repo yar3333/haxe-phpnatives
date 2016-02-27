9296bc453b972d9dbfa731317fc4824dcbeaef54
Yaroslav Sivakov <yar3333@gmail.com>
2016-02-28 01:39:13 +0300

    * pcre: fix rest arguments

--- library/php/PcreNatives.hx
+++ library/php/PcreNatives.hx
@@ -8,13 +8,9 @@ class PcreNatives
 	
 	public static inline function preg_last_error() : Int return untyped __call__('preg_last_error');
 	
-	public static inline function preg_match_all(pattern:String, subject:String, ?matches:NativeArray) : Int return untyped __call__('preg_match_all', pattern, subject, matches);
+	public static inline function preg_match_all(pattern:String, subject:String, ?matches:NativeArray, ?flags:Int, offset=0) : Int return untyped __call__('preg_match_all', pattern, subject, matches, flags, offset);
 	
-	public static inline function preg_match_all_ex(pattern:String, subject:String, ?matches:NativeArray, flags:Int, offset=0) : Int return untyped __call__('preg_match_all', pattern, subject, matches, flags, offset);
-	
-	public static inline function preg_match(pattern:String, subject:String) : Int return untyped __call__('preg_match', pattern, subject);
-	
-	public static inline function preg_match_ex(pattern:String, subject:String, matches:NativeArray, flags=0, offset=0) : Int return untyped __call__('preg_match', pattern, subject, matches, flags, offset);
+	public static inline function preg_match(pattern:String, subject:String, ?matches:NativeArray, flags=0, offset=0) : Int return untyped __call__('preg_match', pattern, subject, matches, flags, offset);
 	
 	public static inline function preg_quote(str:String, ?delimiter:String) : String return untyped __call__('preg_quote', str, delimiter);
 	
@@ -22,9 +18,7 @@ class PcreNatives
 	
 	public static inline function preg_replace_callback(pattern:Dynamic, callback:NativeArray->String, subject:Dynamic, ?limit:Int=-1, ?count:Int) : Dynamic return untyped __call__('preg_replace_callback', pattern, callback, subject, limit, count);
 	
-	public static inline function preg_replace<T>(pattern:Dynamic, replacement:Dynamic, subject:T, ?limit:Int=-1) : T return untyped __call__('preg_replace', pattern, replacement, subject, limit);
-	
-	public static inline function preg_replace_ex<T>(pattern:Dynamic, replacement:Dynamic, subject:T, ?limit:Int=-1, count:Int) : T return untyped __call__('preg_replace', pattern, replacement, subject, limit, count);
+	public static inline function preg_replace(pattern:Dynamic, replacement:Dynamic, subject:T, ?limit:Int=-1, ?count:Int) : T return untyped __call__('preg_replace', pattern, replacement, subject, limit, count);
 	
 	public static inline function preg_split(pattern:String, subject:String, ?limit:Int=-1, flags=0) : NativeArray return untyped __call__('preg_split', pattern, subject, limit, flags);
 }
