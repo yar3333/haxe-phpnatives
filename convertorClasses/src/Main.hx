92da5dee97388dfd3b8bac2161dff834bc306044
Yaroslav Sivakov <yar3333@gmail.com>
2016-03-02 04:48:20 +0300

    * making

--- convertorClasses/src/Main.hx
+++ convertorClasses/src/Main.hx
@@ -16,24 +16,34 @@ typedef Element =
 
 class Main
 {
-	static var groups = [ "array", "var", "imap"  ];
-	
-	static var reID = "[a-zA-Z_][a-zA-Z0-9_]*";
-	static var reNameAndType = "\\s*" + reID + "\\s*[:]\\s*" + reID + "\\s*";
-	static var reArgsStr = "[(]" + reNameAndType + "(?:,\\s*" + reNameAndType + ")*[)]";
-	
 	static function main()
 	{
-		processClasses("calendar", "php.calendar", []);
+		var args = Sys.args();
+		
+		if (args.length >= 3 && args.length <= 4)
+		{
+			processClasses
+			(
+				args[0],
+				args[1],
+				Path.removeTrailingSlashes(args[2]),
+				args.length > 3 ? args[3].rtrim(";").split(";") : []
+			);
+		}
+		else
+		{
+			Sys.println("Arguments: <jsonFile> <outPackage> <outDir> [ <imports> ]");
+		}
+		
 	}
 	
-	static function processClasses(group:String, pack:String, imports:Array<String>)
+	static function processClasses(jsonFile:String, pack:String, outDir:String, imports:Array<String>)
 	{
-		Log.start("Process " + group);
+		Log.start("Process " + jsonFile);
 		
 		var methods = [];
 		
-		var text = File.getContent("bin/" + group + ".json");
+		var text = File.getContent(jsonFile);
 		var elements : Array<Element> = TJSON.parse(text);
 		for (element in elements)
 		{
@@ -42,17 +52,17 @@ class Main
 				var name = element.name.replace("The ", "").replace(" class", "").trim();
 				var isInterface = name.indexOf(" interface") >= 0;
 				if (isInterface) name = name.replace(" interface", "");
-				processClass(isInterface, pack, name, element.body, imports);
+				processClass(isInterface, pack, name, element.body, outDir, imports);
 			}
 		}
 		
-		FileSystem.deleteFile("bin/temp.php");
-		FileSystem.deleteFile("bin/temp.hx");
+		FileSystem.deleteFile("temp.php");
+		FileSystem.deleteFile("temp.hx");
 		
 		Log.finishSuccess();
 	}
 	
-	static function processClass(isInterface:Bool, pack:String, klass:String, body:String, imports:Array<String>)
+	static function processClass(isInterface:Bool, pack:String, klass:String, body:String, outDir:String, imports:Array<String>)
 	{
 		Log.start("Convert " + klass);
 		
@@ -62,16 +72,16 @@ class Main
 		var title = reTitle.replace(doc.find(">root>.classsynopsisinfo")[0].innerText.trim());
 		
 		var fields = doc.find(">root>.fieldsynopsis").map.fn(_.innerText.replace("\n", " "));
-		File.saveContent("bin/temp.php", fields.join("\n").trim());
-		Sys.command("haxelib", [ "run", "refactor", "convertFile", "bin/temp.php", "bin/temp.hx", "c-like_php_fields_to_haxe.rules" ]);
-		fields = File.getContent("bin/temp.hx").split("\n");
+		File.saveContent("temp.php", fields.join("\n").trim());
+		Sys.command("haxelib", [ "run", "refactor", "convertFile", "temp.php", "temp.hx", "c-like_php_fields_to_haxe.rules" ]);
+		fields = File.getContent("temp.hx").split("\n");
 		
 		var methods = doc.find(">root>.constructorsynopsis, >root>.methodsynopsis").map.fn(_.innerText.replace("\n", " "));
-		File.saveContent("bin/temp.php", methods.join("\n").trim());
-		Sys.command("haxelib", [ "run", "refactor", "convertFile", "bin/temp.php", "bin/temp.hx", "c-like_php_methods_to_haxe.rules" ]);
-		methods = File.getContent("bin/temp.hx").split("\n");
+		File.saveContent("temp.php", methods.join("\n").trim());
+		Sys.command("haxelib", [ "run", "refactor", "convertFile", "temp.php", "temp.hx", "c-like_php_methods_to_haxe.rules" ]);
+		methods = File.getContent("temp.hx").split("\n");
 		
-		var dir = "../library/" + pack.replace(".", "/");
+		var dir = outDir + "/" + pack.replace(".", "/");
 		FileSystem.createDirectory(dir);
 		
 		var fieldsStr = fields.map.fn(_ + "\n").join("").trim();
