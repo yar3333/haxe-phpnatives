95fca110a60ecdcdfb0051a310f4b712f5f77ae3
Yaroslav Sivakov <yar3333@gmail.com>
2016-04-18 17:18:47 +0300

    * exceptions fixes

--- library/php/exceptions/InvalidArgumentException.hx
+++ library/php/exceptions/InvalidArgumentException.hx
@@ -3,4 +3,5 @@ package php.exceptions;
 @:native("InvalidArgumentException")
 extern class InvalidArgumentException extends LogicException
 {
+	public function new(?message : String, ?code : Int) : Void;
 }
--- library/php/exceptions/LogicException.hx
+++ library/php/exceptions/LogicException.hx
@@ -3,4 +3,5 @@ package php.exceptions;
 @:native("LogicException")
 extern class LogicException extends Exception
 {
+	public function new(?message : String, ?code : Int) : Void;
 }
